e81e5638783d147be512d26f5bdd3c403a081f34
Yuliia M <myuliia2018@gmail.com>
2022-04-25 11:55:13 -0500

    creat function for all processes in javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -65,3 +65,157 @@ const questions = [
         correctAnswer: "3"
     }
 ];
+// TIMER FUNCTION 
+function setTime() {
+    let timerInterval = setInterval(function () {
+        secondsLeft--;
+        time.textContent = `Time:${secondsLeft}s`;
+
+        if (secondsLeft === 0 || questionCount === questions.length) {
+            clearInterval(timerInterval);
+            questionsEl.style.display = "none";
+            finalEl.style.display = "block";
+            score.textContent = secondsLeft;
+        }
+    }, 1000);
+}
+
+// QUIZ BEGIN 
+function startQuiz() {
+    codersIntro.style.display = "none";
+    questionsEl.style.display = "block";
+    questionCount = 0;
+
+    setTime();
+    setQuestion(questionCount);
+}
+
+// SET QUESTION 
+function setQuestion(id) {
+    if (id < questions.length) {
+        questionEl.textContent = questions[id].question;
+        ans1Btn.textContent = questions[id].answers[0];
+        ans2Btn.textContent = questions[id].answers[1];
+        ans3Btn.textContent = questions[id].answers[2];
+        ans4Btn.textContent = questions[id].answers[3];
+    }
+}
+
+// EVENT FUNCTION CHECK ANSWERS 
+function checkAnswer(event) {
+    event.preventDefault();
+
+    //RIGHT OR WRONG
+    correctWrong.style.display = "block";
+    let p = document.createElement("p");
+    correctWrong.appendChild(p);
+
+    // DISPLAY NEW ELEMENT FOR ANY AMOUNR OF TIME
+    setTimeout(function () {
+        p.style.display = 'none';
+    }, 1000);
+
+    // CONDITIONAL STATEMENTS CORRECT
+    if (questions[questionCount].correctAnswer === event.target.value) {
+        p.textContent = "Correct!";
+    } 
+   
+     //CONDITIONAL STATEMENTS WRONG
+    else if (questions[questionCount].correctAnswer !== event.target.value) {
+        secondsLeft = secondsLeft - 10;
+        p.textContent = "Wrong!";
+    }
+
+    // CYCLE 
+    if (questionCount < questions.length) {
+        questionCount++;
+    }
+    setQuestion(questionCount);
+}
+
+function addScore(event) {
+    event.preventDefault();
+
+    finalEl.style.display = "none";
+    highscoresEl.style.display = "block";
+
+    let init = initialsInput.value.toUpperCase();
+    scoreList.push({ initials: init, score: secondsLeft });
+
+    // HIGH SCORE SORTING LIST
+    scoreList = scoreList.sort((a, b) => {
+        if (a.score < b.score) {
+          return 1;
+        } else {
+          return -1;
+        }
+      });
+    
+    scoreListEl.innerHTML="";
+    for (let i = 0; i < scoreList.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = `${scoreList[i].initials}: ${scoreList[i].score}`;
+        scoreListEl.append(li);
+    }
+
+    // STORAGE OF SCORE 
+    storeScores();
+    displayScores();
+}
+
+function storeScores() {
+    localStorage.setItem("scoreList", JSON.stringify(scoreList));
+}
+
+function displayScores() {
+    // Parsing the JSON string to an object
+    let storedScoreList = JSON.parse(localStorage.getItem("scoreList"));
+
+   
+    if (storedScoreList !== null) {
+        scoreList = storedScoreList;
+    }
+}
+
+// CLEAR THE STORE
+function clearScores() {
+    localStorage.clear();
+    scoreListEl.innerHTML="";
+}
+
+// START OFF ALL EVENT 
+
+start.addEventListener("click", startQuiz);
+
+// CHECK ANSER LISTENER EVENT
+ansBtn.forEach(item => {
+    item.addEventListener('click', checkAnswer);
+});
+
+// ADDING A SCORE EVENT
+submitScrBtn.addEventListener("click", addScore);
+
+// GO BACK LISTENER EVENT FUNCTIN 
+goBackBtn.addEventListener("click", function () {
+    highscoresEl.style.display = "none";
+    codersIntro.style.display = "block";
+    secondsLeft = 75;
+    time.textContent = `Time:${secondsLeft}s`;
+});
+
+// CLEAR SCORE
+clearScrBtn.addEventListener("click", clearScores);
+
+// HIGH SCORE BUTTON ALERT AND DISPLAY LISTENER EVENT
+viewScrBtn.addEventListener("click", function () {
+    if (highscoresEl.style.display === "none") {
+        highscoresEl.style.display = "block";
+    } 
+    else if (highscoresEl.style.display === "block") {
+        highscoresEl.style.display = "none";
+    } 
+    
+    else {
+        return alert("Take Quiz");
+    }
+});
