ab083f9614e929f22b7101bfe63ce4a3d0822c97
Yuliia M <myuliia2018@gmail.com>
2022-04-25 11:47:21 -0500

    adding questions and answers to js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,3 +40,28 @@ const ans2Btn = document.querySelector("#answer-2");
 const ans3Btn = document.querySelector("#answer-3");
 const ans4Btn = document.querySelector("#answer-4");
 
+//Quiz questions/answers
+
+const questions = [ 
+   
+    {
+        question: "The condition in an if / else statement is enclosed within _______.",
+        answers: ["1. quotes", "2. curly brackets", "3. parentheses", "4. square brackets"],
+        correctAnswer: "1"
+    },
+    {
+        question: "Arrays in Javascript can be used to store ______.",
+        answers: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
+        correctAnswer: "3"
+    },
+    {
+        question: "String values must be enclosed within ________ when being assigned to variables.",
+        answers: ["1. commmas", "2. curly brackets", "3. quotes", "4. parentheses"],
+        correctAnswer: "2"
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        answers: ["1. Javascript", "2. terminal/bash", "3. for loops", "4. console.log"],
+        correctAnswer: "3"
+    }
+];
